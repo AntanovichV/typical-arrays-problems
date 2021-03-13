fee5e488f054488bedbcb4f866502bd27e5e02ca
Valentina Antanovich <valentina8antanovich@gmail.com>
2021-03-14 00:45:35 +0300

    task typical-arrays-problems solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
+  if (typeof(array) == "object" && array.length != 0) {
+    return Math.min(...array);
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (typeof(array) == "object" && array.length != 0) {
+    return Math.max(...array);
+  } 
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof(array) == "object" && array.length != 0) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum/array.length;
+}
+return 0;
 }
